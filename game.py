54381a34d5ab5a293d02db1c75d9e40dfac1a977
Maks <40409633+maksymilianczajkowski@users.noreply.github.com>
2023-10-02 03:56:07 +0200

    Update game.py

--- game.py
+++ game.py
@@ -29,10 +29,10 @@ def input_your_choose():
 
 def it_is_time_for_end_the_game():
     while True:
-        choose = input('Do You want to play again? [Y/N]')
-        if choose.lower() in ['n', 'no']:
+        choice = input('Do You want to play again? [Y/N]')
+        if choice.lower() in ['n', 'no']:
             return True
-        elif choose.lower() in ['y', 'yes']:
+        elif choice.lower() in ['y', 'yes']:
             return False
 
 def s_just_for_a_better_look( variable ):
@@ -41,10 +41,13 @@ def s_just_for_a_better_look( variable ):
 while True:
     #player's choice
     player_choice = input_your_choose()
+  
     #Get comp choice
     comp_choice = random.choice( list(outcomes_dict.keys()) )
-    #Print players choose
+  
+    #Print players choice
     print('[PLAYER] '+player_choice.upper()+' versus '+comp_choice.upper()+' [CPU]')
+  
     #compute who wins
     if player_choice in outcomes_dict[comp_choice]:
         print('CPU won this round!')
@@ -55,6 +58,7 @@ while True:
     else:
         print('Draw! (nobody win)')
         draw += 1
+      
     #Ask for next round
     if it_is_time_for_end_the_game():
         #If player don't want to play again, show the result
